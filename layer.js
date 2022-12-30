b2c6bf525e21edcd018e3323de0c12a8f2a940df
BlurryKnight <80977669+BlurryKnight@users.noreply.github.com>
2022-12-30 21:51:03 +0100

    Update layer.js

--- layer.js
+++ layer.js
@@ -8,18 +8,18 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.set_layer = function(sprite, layer) {
-        // Set the layer of the sprite to the specified value
-        sprite.layer = layer;
+    ext.get_layer = function(sprite) {
+        // Return the current layer of the sprite
+        return sprite.layer;
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            [' ', 'set layer of %s to %n', 'set_layer', 'Sprite1', 1]
+            ['r', 'layer of %s', 'get_layer', 'Sprite1']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Set Layer Extension', descriptor, ext);
+    ScratchExtensions.register('Get Layer Extension', descriptor, ext);
 })({});
