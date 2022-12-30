53b04928dfc482a116fd70acdc8a88ade27d270c
BlurryKnight <80977669+BlurryKnight@users.noreply.github.com>
2022-12-30 21:45:35 +0100

    Update layer.js

--- layer.js
+++ layer.js
@@ -1,71 +1,25 @@
-(function (Scratch) {
-  'use strict';
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
 
-  // Define a function to implement the layer behavior for a target
-  const implementLayerForTarget = (target, originalTarget) => {
-    // Define a symbol to store the layer value for each target
-    const LAYER = Symbol('layer');
-
-    // Initialize the layer value for the target
-    target[LAYER] = originalTarget ? originalTarget[LAYER] : 0;
-
-    // Override the _getRenderOrder method to return the target's layer value
-    const original = target._getRenderOrder;
-    target._getRenderOrder = function () {
-      return this[LAYER];
-    };
-  };
-
-  // Wait for the Scratch.VM object to be defined
-  window.addEventListener('scratchLoaded', () => {
-    // Get a reference to the vm object
-    const vm = new Scratch.VM();
-
-    // Implement the layer behavior for all existing targets
-    vm.runtime.targets.forEach((target) => implementLayerForTarget(target));
-
-    // Implement the layer behavior for any new targets that are created
-    vm.runtime.on('targetWasCreated', (target, originalTarget) => implementLayerForTarget(target, originalTarget));
-
-    // Implement the layer behavior for all targets when a project is loaded
-    vm.runtime.on('PROJECT_LOADED', () => {
-      vm.runtime.targets.forEach((target) => implementLayerForTarget(target));
-    });
-
-    // Define the extension's blocks
-    const blocks = {
-      setLayer: {
-        opcode: 'setLayer',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'set layer to [LAYER]',
-        arguments: {
-          LAYER: {
-            type: Scratch.ArgumentType.NUMBER,
-            defaultValue: 0,
-          },
-        },
-      },
-      getLayer: {
-        opcode: 'getLayer',
-        blockType: Scratch.BlockType.REPORTER,
-        text: 'layer',
-        disableMonitor: true,
-      },
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
     };
 
-    // Define the extension's functions
-    const functions = {
-      setLayer: function (args, util) {
-        // Set the target's layer value to the specified value
-        util.target[LAYER] = +args.LAYER || 0;
-      },
-      getLayer: function (args, util) {
-        // Return the target's layer value
-        return util.target[LAYER];
-      },
+    ext.set_layer = function(sprite, layer) {
+        // Set the layer of the sprite to the specified value
+        sprite.layer = layer;
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            [' ', 'set layer of %s to %n', 'set_layer', 'Sprite1', 1]
+        ]
     };
 
     // Register the extension
-    Scratch.extensions.register(new Scratch.Extension(blocks, functions));
-  });
-})(Scratch);
+    ScratchExtensions.register('Set Layer Extension', descriptor, ext);
+})({});
