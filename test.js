91ac1d3a052c4dc165a023eb1e90dbcff1fe0c0f
BlurryKnight <80977669+BlurryKnight@users.noreply.github.com>
2022-12-30 21:24:20 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,42 +1,64 @@
-class MyExtension {
-  getInfo() {
-    return {
-      id: 'myextensionexample',
-      name: 'Cool Extension',
-      blocks: [
-        {
-          opcode: 'sayHello',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'Say Hello'
-        },
-        {
-          opcode: 'clearScreen',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'Clear Screen'
-        }
-      ]
+(function (Scratch) {
+  'use strict';
+
+  const LAYER = Symbol('layer');
+
+  /**
+   * @param {VM.RenderedTarget} target
+   */
+  const implementLayerForTarget = (target, originalTarget) => {
+    target[LAYER] = originalTarget ? originalTarget[LAYER] : 0;
+
+    const original = target._getRenderOrder;
+    target._getRenderOrder = function () {
+      return this[LAYER];
     };
+  };
+
+  const vm = Scratch.vm;
+  vm.runtime.targets.forEach((target) => implementLayerForTarget(target));
+  vm.runtime.on('targetWasCreated', (target, originalTarget) => implementLayerForTarget(target, originalTarget));
+  vm.runtime.on('PROJECT_LOADED', () => {
+    vm.runtime.targets.forEach((target) => implementLayerForTarget(target));
+  });
+
+  class Layer {
+    getInfo() {
+      return {
+        id: 'layer',
+        name: 'Layer',
+        color1: '#4287f5',
+        color2: '#2b62ba',
+        color3: '#204785',
+        blocks: [
+          {
+            opcode: 'setLayer',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'set layer to [LAYER]',
+            arguments: {
+              LAYER: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 0,
+              },
+            },
+          },
+          {
+            opcode: 'getLayer',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'layer',
+            disableMonitor: true,
+          },
+        ],
+      };
+    }
+    setLayer(args, util) {
+      // TODO: move to Scratch.Cast when it's merged
+      util.target[LAYER] = +args.LAYER || 0;
+    }
+    getLayer(args, util) {
+      return util.target[LAYER];
+    }
   }
 
-  sayHello() {
-    // Create a new div element
-    let div = document.createElement('div');
-    // Set the text to display
-    div.innerText = 'Hello, world!';
-    // Style the div so it's positioned in the corner of the screen
-    div.style.position = 'absolute';
-    div.style.top = 0;
-    div.style.right = 0;
-    // Append the div to the body of the page
-    document.body.appendChild(div);
-  }
-
-  clearScreen() {
-    // Select all div elements on the page
-    let divs = document.querySelectorAll('div');
-    // Remove each div
-    divs.forEach(div => div.remove());
-  }
-}
-
-Scratch.extensions.register(new MyExtension());
+  Scratch.extensions.register(new Layer());
+})(Scratch);
